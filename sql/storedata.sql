4f76f59710abfb725eba4cf6a48aa628d11bee47
jakkti12 <jakkti005@gmail.com>
2023-12-13 09:58:08 +0700

    add_DB

---
+++ sql/storedata.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 13, 2023 at 02:57 AM
+-- Server version: 8.0.31
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `storedata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(200) NOT NULL,
+  `product_amount` varchar(100) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_amount`) VALUES
+(53, 'ปรินเตอร์', '55'),
+(58, 'นมสด', '55'),
+(59, 'น้ำ', '55'),
+(60, 'ปลักไฟฟ้า', '2'),
+(61, 'ขนม1', '5'),
+(62, 'ขนม2', '1'),
+(63, 'ขนม3', '1'),
+(64, 'เตา', '1'),
+(65, '123', '123'),
+(66, '123', '123'),
+(67, '123', '123'),
+(68, '123', '123'),
+(69, '123', '123'),
+(70, '123', '123'),
+(71, '123', '123'),
+(72, '123', '123'),
+(73, '123', '123'),
+(74, '123', '123'),
+(75, '123', '123'),
+(76, '123', '123'),
+(77, '123', '123'),
+(78, '123', '123'),
+(79, '123', '123'),
+(80, '123', '123'),
+(81, '123', '123'),
+(82, '123', '123'),
+(83, '123', '123'),
+(84, '123', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `site_title` varchar(50) NOT NULL,
+  `timezone` varchar(100) NOT NULL,
+  `recaptcha` varchar(5) NOT NULL,
+  `theme` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `site_title`, `timezone`, `recaptcha`, `theme`) VALUES
+(1, 'Dnato System Login', 'Asia/Makassar', 'no', 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+DROP TABLE IF EXISTS `tokens`;
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  `created` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `password` text NOT NULL,
+  `last_login` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `banned_users` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `role`, `password`, `last_login`, `status`, `banned_users`) VALUES
+(1, 'admin@gmail.com', 'Admin', 'Admin', '1', 'sha256:1000::RDSzmFu7FdqnVLMmCrX89yNnKeZQ3T7F', '2023-12-12 03:20:06 AM', 'approved', 'unban');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
