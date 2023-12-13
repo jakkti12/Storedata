099abb27555da91de3cc7dd23ceb0d3f4ec436a9
jakkti12 <jakkti005@gmail.com>
2023-12-13 13:39:39 +0700

    update no database

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -3,13 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Homepage extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->library('session');
-		$data = $this->session->userdata;
-	}
-
 	public function index()
 	{
 		$this->load->view('homeview');
