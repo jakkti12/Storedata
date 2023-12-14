14a2cd7b8fc1070876718d6787726caefa86b8f3
jakkti12 <jakkti005@gmail.com>
2023-12-14 09:24:54 +0700

    up homepage

--- application/views/homeview.php
+++ application/views/homeview.php
@@ -1,8 +1,17 @@
-<?php
-echo 'hello';
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php if (empty($this->session->userdata['email'])) {
-} else { ?>
-            <a href="<?php echo base_url() . 'logout' ?>">Log Out</a>
-<?php } ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <h1>hello</h1>
+<body>
+    <?php if (empty($this->session->userdata['email'])) {
+    } else { ?>
+        <a href="<?php echo base_url() . 'logout' ?>">Log Out</a>
+    <?php } ?>
+</body>
+
+</html>
