c8322a84d3cf81857ff465b7ef8b49f6d9180f3c
jakkti12 <jakkti005@gmail.com>
2023-12-14 10:46:15 +0700

    index ui

--- application/views/auth/register.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <?php $fattr = array('class' => 'form-signin');
-    echo form_open(site_url() . 'register', $fattr); ?>
-    <input type="text" name="fristname" placeholder="Frist Name">
-    <input type="text" name="lastname" placeholder="Last Name">
-    <input type="submit">
-</div>
